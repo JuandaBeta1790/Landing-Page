7060b7b065f3119399ea8e18be256506e95c7fb2
Juandabeta1790 <juan.betancur@telemetrik.com.co>
2024-12-18 08:25:18 -0500

    Mover Ocean a publi

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: "https://vbartalis.github.io"
-  
+  site: "https://JuandaBeta1790.github.io/Landing-Page",
+  base: "", // Cambia 'Landing-Page' por el nombre de tu repositorio en GitHub
 });
