aa943ecc5546338450db0787dee44858d1e4143f
Juandabeta1790 <juan.betancur@telemetrik.com.co>
2025-01-09 08:18:50 -0500

    Agregado archivo de declaración de tipos y actualizado tsconfig.json

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,12 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+// src/types/env.d.ts (o donde tengas este archivo)
+declare global {
+    interface Window {
+      dataLayer: any[];
+    }
+  }
+  
+  // Asegúrate de que el archivo se considere un módulo
+  export {};
+  
